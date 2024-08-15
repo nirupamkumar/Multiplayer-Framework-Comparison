99a0a67a0ab6997330a398e7c8b79195c928ebea
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-08-15 23:12:09 +0200

    Initial commit: Set up Unity client and Visual Studio console server for WebSocket implementation

---
+++ 01_Custom_WebSocket/WebSocket_Server/WebSocket_Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebSocket_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
