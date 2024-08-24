b1555ef7841507b80b190a3ab3065fe6a73b26da
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-08-24 22:13:04 +0200

    [Netcode] Add GameManager.cs

---
+++ 03_Unity_Netcode/Netcode_MUD_ClientServer/Assets/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public int[] worldData;
+
+    private void Start()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+            CreateWorld();
+        }
+    }
+
+    private void CreateWorld()
+    {
+        foreach (var data in worldData)
+        {
+            // Instantiate and network-spawn world objects based on data
+        }
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        var playerInstance = Instantiate(playerPrefab);
+        playerInstance.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
+    }
+
+    private void OnDestroy()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        }
+    }
+}
